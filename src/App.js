60069d537d91f4dd5368447cb9d71091aefb5ed1
volkanb <volkanbenli92@gmail.com>
2021-01-16 22:30:38 -0500

    Sets up Accordion component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import Accordion from './components/Accordion';
 
-export default () => {
-	return <h1>Widgets App</h1>;
+const items = [
+	{
+		title: 'What is React?',
+		content: 'React is a front end javascript framework',
+	},
+	{
+		title: 'Why use React?',
+		content: 'React is a favorite JS library among engineers',
+	},
+	{
+		title: 'How do you use React?',
+		content: 'You use React by creating components',
+	},
+];
+
+const App = () => {
+	return (
+		<div>
+			<Accordion items={items} />
+		</div>
+	);
 };
+
+export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Accordion = ({ items }) => {
+	return <h1>Accordion length {items.length}</h1>;
+};
+
+export default Accordion;
