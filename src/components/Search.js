b86262cc4706d041b9eaf750036318a4aafcf6c6
volkanb <volkanbenli92@gmail.com>
2021-02-02 13:13:02 -0500

    Adds axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
 	const [term, setTerm] = useState('');
+
+	useEffect(() => {
+		const search = async () => {
+			await axios.get('asdf');
+		};
+
+		search();
+	}, [term]);
 
 	return (
 		<div>
